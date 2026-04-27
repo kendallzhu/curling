27ccad28a3435b43e1b664dfc726a88e6b9e75f6
Sam Heil <sheil3677@gmail.com>
2026-04-26 21:41:40 -0400

    .

--- nn.py
+++ nn.py
@@ -13,9 +13,7 @@ class Layer(ABC):
         pass
 
     @abstractmethod
-    def update_and_return_input_gradient(
-        self, output_gradient: np.array, learning_rate: float, regularization: float
-    ):
+    def get_gradients(self, output_gradient: np.array):
         pass
 
 
@@ -25,24 +23,30 @@ class Linear(Layer):
         n_out, n_in = self.weights.shape
         self.previous_inputs = np.zeros(n_in)
         self.output_gradient = np.zeros(n_out)
+        self.bias = np.zeros(n_out)
 
     def run(self, inputs: np.array):
         self.previous_inputs = inputs
-        return self.weights @ inputs
+        return self.weights @ inputs + self.bias
 
-    def update_and_return_input_gradient(
-        self, output_gradient: np.array, learning_rate: float, regularization: float
-    ):
+    def get_gradients(self, output_gradient: np.array):
         self.output_gradient = output_gradient
-        n_out, n_in = self.weights.shape
         input_gradient = self.weights.T @ output_gradient
-        self.weights -= (
-            learning_rate
-            * output_gradient.reshape((n_out, 1))
-            @ self.previous_inputs.reshape((1, n_in))
-            + regularization * self.weights
-        )
-        return input_gradient
+        n_out, n_in = self.weights.shape
+        weight_gradient = output_gradient.reshape(
+            (n_out, 1)
+        ) @ self.previous_inputs.reshape((1, n_in))
+        return input_gradient, weight_gradient, output_gradient
+
+    def update_weights(
+        self,
+        weight_gradient: np.array,
+        bias_gradient: np.array,
+        learning_rate: float,
+        regularization: float,
+    ):
+        self.weights -= learning_rate * weight_gradient + regularization * self.weights
+        self.bias -= bias_gradient
 
 
 class Max0(Layer):
@@ -51,12 +55,9 @@ class Max0(Layer):
         self.weights = np.array([])
         return np.fmax(inputs, 0)
 
-    def update_and_return_input_gradient(
-        self, output_gradient: np.array, learning_rate: float, regularization: float
-    ):
-        del learning_rate, regularization
+    def get_gradients(self, output_gradient: np.array):
         self.output_gradient = output_gradient
-        return np.where(self.previous_inputs > 0, 1, 0) * output_gradient
+        return np.where(self.previous_inputs > 0, 1, 0) * output_gradient, None, None
 
 
 class MapTo01(Layer):
@@ -65,25 +66,130 @@ class MapTo01(Layer):
         self.weights = np.array([])
         return np.exp(inputs) / (1 + np.exp(inputs))
 
-    def update_and_return_input_gradient(
-        self, output_gradient: np.array, learning_rate: float, regularization: float
-    ):
-        del learning_rate, regularization
+    def get_gradients(self, output_gradient: np.array):
         self.output_gradient = output_gradient
         return (
-            np.exp(self.previous_inputs) / (1 + np.exp(self.previous_inputs)) ** 2
-        ) * output_gradient
+            (np.exp(self.previous_inputs) / (1 + np.exp(self.previous_inputs)) ** 2)
+            * output_gradient,
+            None,
+            None,
+        )
 
 
 class NN:
     def __init__(self, layers: list[Layer]):
         self.layers = layers
 
+    def debug_gradients(
+        self, inputs: np.array, actual: np.array, loss_function: object
+    ):
+        prediction = self.run(inputs)
+        initial_output_gradient = loss_function.output_gradient(prediction, actual)
+        input_gradients_by_layer = [None for i in self.layers]
+        weights_gradients_by_layer = [None for i in self.layers]
+        output_gradient = initial_output_gradient
+        for layer_idx, layer in reversed(list(enumerate(self.layers))):
+            input_gradient, weights_gradient, bias_gradient = layer.get_gradients(
+                output_gradient
+            )
+            output_gradient = input_gradient
+            input_gradients_by_layer[layer_idx] = input_gradient
+            weights_gradients_by_layer[layer_idx] = weights_gradient
+
+        return {
+            "inputs": inputs,
+            "prediction": prediction,
+            "actual": actual,
+            "initial_output_gradient": initial_output_gradient,
+            "input_gradients_by_layer": input_gradients_by_layer,
+            "weights_gradients_by_layer": weights_gradients_by_layer,
+        }
+
     def run(self, inputs: np.array):
         values = inputs
         for layer in self.layers:
             values = layer.run(values)
         return values
+
+    def get_gradients(self, output_gradient: np.array) -> list[np.array]:
+        weights_gradients_by_layer = []
+        bias_gradients_by_layer = []
+        for layer in reversed(self.layers):
+            input_gradient, weights_gradient, bias_gradient = layer.get_gradients(
+                output_gradient
+            )
+            output_gradient = input_gradient
+            weights_gradients_by_layer.append(weights_gradient)
+            bias_gradients_by_layer.append(bias_gradient)
+        return weights_gradients_by_layer[::-1], bias_gradients_by_layer[::-1]
+
+    def get_average_loss(
+        self, input_features: np.array, answers: np.array, loss_function: object
+    ):
+        losses = []
+        for k in range(input_features.shape[0]):
+            inputs = input_features[k, :]
+            actual = answers[k]
+            prediction = self.run(inputs)
+            losses.append(loss_function.get_loss(prediction, actual))
+        return np.average(np.array(losses))
+
+    def train(
+        self,
+        input_features: np.array,  # (# examples, # columns),
+        answers: np.array,  # (# examples)
+        loss_function: object,
+        learning_rate: float,
+        regularization: float,
+    ):
+        weight_gradients_by_input = []
+        bias_gradients_by_input = []
+        losses = []
+        for k in range(input_features.shape[0]):
+            inputs = input_features[k, :]
+            actual = answers[k]
+            prediction = self.run(inputs)
+            output_gradient = loss_function.output_gradient(prediction, actual)
+            losses.append(loss_function.get_loss(prediction, actual))
+            weight_gradients, bias_gradients = self.get_gradients(output_gradient)
+            weight_gradients_by_input.append(weight_gradients)
+            bias_gradients_by_input.append(bias_gradients)
+
+        weight_gradients_by_layer = [[] for _ in self.layers]
+        bias_gradients_by_layer = [[] for _ in self.layers]
+
+        for this_input_gradients in weight_gradients_by_input:
+            for layer_idx, input_gradient_for_layer in enumerate(this_input_gradients):
+                weight_gradients_by_layer[layer_idx].append(input_gradient_for_layer)
+        for this_input_gradients in bias_gradients_by_input:
+            for layer_idx, input_gradient_for_layer in enumerate(this_input_gradients):
+                bias_gradients_by_layer[layer_idx].append(input_gradient_for_layer)
+
+        # print(gradients_by_input)
+        for (
+            layer,
+            weight_layer_gradients_by_input,
+            bias_layer_gradients_by_input,
+        ) in zip(self.layers, weight_gradients_by_layer, bias_gradients_by_layer):
+            if isinstance(layer, Linear):
+                total_weight_layer_gradient = np.zeros(
+                    weight_layer_gradients_by_input[0].shape
+                )
+                for layer_gradient in weight_layer_gradients_by_input:
+                    #           print(layer_gradient)
+                    total_weight_layer_gradient += layer_gradient
+                total_bias_layer_gradient = np.zeros(
+                    bias_layer_gradients_by_input[0].shape
+                )
+                for layer_gradient in bias_layer_gradients_by_input:
+                    total_bias_layer_gradient += layer_gradient
+                layer.update_weights(
+                    total_weight_layer_gradient / len(answers),
+                    total_bias_layer_gradient / len(answers),
+                    learning_rate,
+                    regularization,
+                )
+        return np.average(np.array(losses))
 
     def update(
         self, output_gradient: np.array, learning_rate: float, regularization: float
@@ -92,7 +198,8 @@ class NN:
             output_gradient = layer.update_and_return_input_gradient(
                 output_gradient, learning_rate, regularization
             )
-            logger.debug(f"new output gradient: {output_gradient}")
+
+    #            logger.debug(f"new output gradient: {output_gradient}")
 
     def debug_print(self):
         print("calling debug_print")
@@ -129,3 +236,13 @@ class SquaredErrorLoss:
 
     def output_gradient(self, prediction, actual):
         return 2 * (prediction - actual)
+
+
+class CrossEntropyLoss:
+    def get_loss(self, prediction, actual):
+        prediction = np.clip(prediction, 1e-7, 1 - 1e-7)
+        return -(actual * np.log(prediction) + (1 - actual) * np.log(1 - prediction))
+
+    def output_gradient(self, prediction, actual):
+        prediction = np.clip(prediction, 1e-7, 1 - 1e-7)
+        return prediction - actual  # sigmoid saturation cancels out cleanly
