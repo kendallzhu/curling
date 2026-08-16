b7007275e8b9b3987e5e45f4ff6ca9f1abb56bee
Kendall Zhu <kendallzhu66@gmail.com>
2026-08-16 14:55:19 -0400

    move random sheet state to data generation

--- presets.py
+++ presets.py
@@ -1,40 +1,6 @@
 import numpy as np
 
-from constants import (
-    center_line_y,
-    center_of_target_house,
-    house_outer_circle_radius,
-)
 from state import SheetStates, Velocities
-
-
-def random_sheet_states(*, team1: int, team2: int, num_sims: int = 1) -> SheetStates:
-    num_team0 = team1
-    num_team1 = team2
-    num_stones = num_team0 + num_team1
-    x = np.zeros((num_sims, num_stones), dtype=float)
-    y = np.empty((num_sims, num_stones), dtype=float)
-
-    angle = np.random.uniform(0.0, 2.0 * np.pi, size=(num_sims, num_stones))
-    radius = house_outer_circle_radius * np.sqrt(np.random.uniform(0.0, 1.0, size=(num_sims, num_stones)))
-
-    version = np.random.random(size=(num_sims, num_stones)) < 0.2
-
-    x = center_of_target_house + np.where(
-    version, -np.random.uniform(2.0, 4.0, size=(num_sims, num_stones)), radius * np.cos(angle))
-    y = np.where(version, np.random.uniform(center_line_y - 1.0, center_line_y + 1.0, size=(num_sims, num_stones)), center_line_y + radius * np.sin(angle))
-
-
-    return SheetStates(
-        first_team=np.zeros(num_sims, dtype=int),
-        x=x,
-        y=y,
-        velocities=Velocities(
-            v=np.zeros((num_sims, num_stones), dtype=float),
-            theta=np.zeros((num_sims, num_stones), dtype=float),
-        ),
-        rotation_directions=np.zeros((num_sims, num_stones), dtype=int),
-    )
 
 
 def demo_collisions_sheet_states() -> SheetStates:
